a5d59386340f4c71570395b73ca070b870dcf5c2
zuhno <zuhno11@gmail.com>
2023-12-14 01:13:37 +0900

    temp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-	std::cout << "Hello World!" << std::endl;
-	return 0; 
+int id = 0;
+
+class Marine {
+    int unit_id;
+    int hp;
+    int coord_x, coord_y;
+    int damage;
+    bool is_dead;
+
+  public:
+    Marine();
+    Marine(int x, int y);
+
+    int attack();
+    void be_attacked(int damage_earn);
+    void move(int x, int y);
+
+    void show_status();
+};
+
+Marine::Marine() {
+  unit_id = ++id;
+  hp = 50;
+  coord_x = coord_y = 0;
+  damage = 5;
+  is_dead = false;
+}
+Marine::Marine(int x, int y) {
+  unit_id = ++id;
+  coord_x = x;
+  coord_y = y;
+  hp = 50;
+  damage = 5;
+  is_dead = false;
+}
+void Marine::move(int x, int y) {
+  coord_x += x;
+  coord_y += y;
+
+  return;
+}
+void Marine::show_status() {
+  std::cout << " *** Marine *** " << std::endl;
+  std::cout << " Unit Id : " << unit_id << std::endl;
+  std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
+            << std::endl;
+  std::cout << " HP : " << hp << std::endl;
+  std::cout << " Dead : " << is_dead << std::endl;
+}
+void Marine::be_attacked(int damage_earn) {
+  hp -= damage_earn;
+  if (hp <= 0) is_dead = true;
+}
+int Marine::attack() {
+  return damage;
+}
+
+int main() {
+  Marine *marines[100];
+  marines[0] = new Marine(2, 3);
+  marines[1] = new Marine(3, 5);
+
+  marines[0]->show_status();
+  marines[1]->show_status();
+
+  std::cout << std::endl
+            << "마린 1 이 마린 2 를 공격!" << std::endl;
+
+  marines[0]->be_attacked(marines[1]->attack());
+
+  marines[0]->show_status();
+  marines[1]->show_status();
+
+  delete marines[0];
+  delete marines[1];
+
+  return 0;
 }
